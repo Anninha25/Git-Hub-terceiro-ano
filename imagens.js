22cdaba0c3ab0f4db137271ebabe1e5fca47fc56
Anninha25 <160169296+Anninha25@users.noreply.github.com>
2024-05-13 14:04:44 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,15 @@
+let Fundo;
+let imagemCarro;
+let imagemCarro1;
+let imagemCarro2;
+let imagemAtor;
+
+
+ function preload(){
+  Fundo = loadImage("imagens/estrada.png");
+  imagemCarro = loadImage("imagens/carros56.png");
+   imagemCarro1 = loadImage("imagens/carros4.png");
+   imagemCarro2 = loadImage("imagens/carros2.png");
+   imagemAtor = loadImage("imagens/pintinho.png");
+  imagemCarros = [imagemCarro, imagemCarro1, imagemCarro2,imagemCarro, imagemCarro1, imagemCarro2];   
+  }
